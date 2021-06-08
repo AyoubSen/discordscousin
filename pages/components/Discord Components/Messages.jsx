abb72d51456b086c5d3c3a9bbc8a3fbd4cb834e3
Ayoub Senhaji <ayoubsenhajidev@gmail.com>
2021-06-08 17:34:48 +0100

    added some tests

--- pages/components/Discord Components/Messages.jsx
+++ pages/components/Discord Components/Messages.jsx
@@ -3,11 +3,14 @@ import { Flex, Box, Text, Center } from "@chakra-ui/react";
 function Messages({ messages, users }) {
   const getColor = (username) => {
     let colorMessage;
-    users.map((user) => {
-      if (user.name === username) {
-        colorMessage = user.color;
-      }
-    });
+    if (users) {
+      users.map((user) => {
+        if (user.name === username) {
+          colorMessage = user.color;
+        }
+      });
+    }
+
     return colorMessage;
   };
 
@@ -19,44 +22,46 @@ function Messages({ messages, users }) {
         h='80vh'
         overflowY='scroll'
       >
-        {messages.map((item) => {
-          return (
-            <Flex margin='20px 0 0 20px'>
-              <Box>
-                <Center
-                  bg={getColor(item.Username)}
-                  w='40px'
-                  color='#000'
-                  h='40px'
-                  borderRadius='50px'
-                  marginRight='10px'
-                  fontWeight='bold'
-                >
-                  {item.Username.slice(0, 1)}
-                </Center>
-              </Box>
-              <Box>
-                <Flex alignItems='center'>
-                  <Text
-                    marginLeft='10px'
-                    fontWeight='bold'
-                    cursor='pointer'
-                    fontSize='15px'
-                    _hover={{ textDecoration: "underline" }}
-                  >
-                    {item.Username}
-                  </Text>
-                  <Text marginLeft='5px' fontSize='12px' color='gray'>
-                    {item.Timestamp
-                      ? new Date(item.Timestamp.toDate()).toUTCString()
-                      : "..."}
-                  </Text>
+        {messages
+          ? messages.map((item) => {
+              return (
+                <Flex margin='20px 0 0 20px'>
+                  <Box>
+                    <Center
+                      bg={getColor(item.Username)}
+                      w='40px'
+                      color='#000'
+                      h='40px'
+                      borderRadius='50px'
+                      marginRight='10px'
+                      fontWeight='bold'
+                    >
+                      {item.Username.slice(0, 1)}
+                    </Center>
+                  </Box>
+                  <Box>
+                    <Flex alignItems='center'>
+                      <Text
+                        marginLeft='10px'
+                        fontWeight='bold'
+                        cursor='pointer'
+                        fontSize='15px'
+                        _hover={{ textDecoration: "underline" }}
+                      >
+                        {item.Username}
+                      </Text>
+                      <Text marginLeft='5px' fontSize='12px' color='gray'>
+                        {item.Timestamp
+                          ? new Date(item.Timestamp.toDate()).toUTCString()
+                          : "..."}
+                      </Text>
+                    </Flex>
+                    <Text marginLeft='10px'>{item.Message}</Text>
+                  </Box>
                 </Flex>
-                <Text marginLeft='10px'>{item.Message}</Text>
-              </Box>
-            </Flex>
-          );
-        })}
+              );
+            })
+          : "no messages yet"}
       </Flex>
     </>
   );
